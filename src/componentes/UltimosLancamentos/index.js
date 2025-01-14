d904938c93a530c28a78a3ce4e1ce2c6edd83f0b
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-01-14 20:55:23 -0300

    card com props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,5 +1,7 @@
 import { livros } from './dadosUltimosLancamentos'
 import { Titulo }  from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
 import styled from 'styled-components'
 
 const UltimosLancamentosContainer = styled.section`
@@ -20,16 +22,26 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo cor="#EB9B00" 
-            tamanhoFonte="36px"
-             alinhamento="center"
-             >ÚLTIMOS LANÇAMENTOS</Titulo>
+            <Titulo 
+                cor="#EB9B00" 
+                tamanhoFonte="36px"
+                alinhamento="center"
+             >
+                ÚLTIMOS LANÇAMENTOS
+            </Titulo>
             <NovosLivrosContainer>
                 { livros.map( livro => (
                     // eslint-disable-next-line jsx-a11y/alt-text
                     <img src={livro.src}/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por..."
+                subtitulo= "Angula 11"
+                descricao= "Construindo uma aplicação com a plataorma Goolgle."
+                img={imagemLivro}
+            />
+            
         </UltimosLancamentosContainer>
     )
 }
