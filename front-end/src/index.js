edc835ebb8cdb78efeea9ff8ecf242512ee0dc65
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-05-03 11:27:58 -0300

    broseroute

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -26,7 +27,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle/>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/favoritos" element={<p>Oi!</p>} />
+        <Route path="/" element={<App />} />
+      </Routes>
+
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
