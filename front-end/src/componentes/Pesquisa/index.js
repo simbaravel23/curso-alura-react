013fdf33d4962e2ddbaafda4f2b52620cfdcd65d
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-06-06 10:08:13 -0300

    fech

--- front-end/src/componentes/Pesquisa/index.js
+++ front-end/src/componentes/Pesquisa/index.js
@@ -1,7 +1,9 @@
 import Input from '../Input'
 import styled from 'styled-components'
+import { getLivros } from '../../servicos/livros'
 import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import {useEffect} from 'react'
+
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -44,7 +46,16 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+    const [ livros, setLivros] = useState ([])
 
+    useEffect(() => {
+      
+    }, [])
+
+    async function fechLivros() {
+         const livrosDaAPI = getLivros()
+         setLivros(livrosDaAPI)
+    }  
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
