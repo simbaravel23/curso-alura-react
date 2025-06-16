fde2f41362f8bde6c6a9b3fad75566d360ffe204
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2025-06-16 14:40:05 -0300

    server fav

---
+++ alura-books-server/servicos/favorito.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+const { deletaLivroPorId } = require("./livro")
+
+function getTodosFavoritos(){
+    return JSON.parse (fs.readFileSync("favoritos.json"))
+}
+
+function deletaFavoritoPorId(id){
+    const livros = JSON.parse (fs.readFileSync("favoritos.json"))
+
+    const livrosFiltrados = livros.filter(livro => livro.id !== id)
+    fs.writeFileSync("favoritos.json", JSON.stringify(livrosFiltrados))
+}
+
+function insereFavorito(id){
+    const livros = JSON.parse (fs.readFileSync("livros.json"))
+    const favoritos = JSON.parse (fs.readFileSync("favoritos.json"))
+ 
+    const livroInserido = livro.find(livro=> livro.id === id)
+    const novaListaDeLivrosFavoritos = [...favoritos,livroInserido]
+    fs.writeFileSync("favoritos.json", JSON.stringify(novaListaDeLivrosFavoritos))
+}
+module.exports = {
+    getTodosFavoritos,
+    deletaLivroPorId,
+    insereFavorito
+}
